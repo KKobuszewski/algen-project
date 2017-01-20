7f0806a45a1bb3099a740ddccb91c3393f8ed158
KKobuszewski <konrad.kobuszewski93@gmail.com>
2017-01-20 17:44:11 +0100

    adding class for convinient FILE* usage

---
+++ utils/files.hpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+
+
+
+class File
+{
+private:
+    
+public:
+    char name[256];
+    char access_mode[2];
+    FILE* stream;
+    
+    File(const char*, const char*);
+    ~File();
+};
+
+File::File(const char* _name, const char* _access_mode)
+{
+    strcpy(name,_name);
+    strcpy(access_mode,_access_mode);
+    
+    stream = fopen(name,access_mode);
+    if (!stream)                        {  fprintf(stderr,"Error! Cannot open file "); exit(EXIT_FAILURE);  }
+}
+
+
+File::~File()
+{
+    if (!stream) fclose(stream);
+}
